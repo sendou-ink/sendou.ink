1e7268c101deadfd3bb1246e924c5eceb395c2ad
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-10-25 21:32:15 +0300

    Progress

--- app/features/tournament-bracket/core/Progression.server.ts
+++ app/features/tournament-bracket/core/Progression.server.ts
@@ -1,6 +1,7 @@
 // todo
 
 import type { Tables, TournamentStageSettings } from "~/db/tables";
+import invariant from "../../../utils/invariant";
 
 export interface DBSource {
 	/** Index of the bracket where the teams come from */
@@ -24,11 +25,12 @@ interface BracketBase {
 
 // Note sources is array for future proofing reasons. Currently the array is always of length 1 if it exists.
 
-interface InputBracket extends BracketBase {
+export interface InputBracket extends BracketBase {
+	id: string;
 	sources?: EditableSource[];
 }
 
-interface ValidatedBracket {
+export interface ValidatedBracket extends BracketBase {
 	sources?: DBSource[];
 }
 
@@ -36,7 +38,7 @@ export type ValidationError =
 	// user written placements can not be parsed
 	| {
 			type: "PLACEMENTS_PARSE_ERROR";
-			bracketIds: string[];
+			bracketId: string;
 	  }
 	// tournament is ending with a format that does not resolve a winner such as round robin or grouped swiss
 	| {
@@ -45,18 +47,22 @@ export type ValidationError =
 	// from each bracket one placement can lead to only one bracket
 	| {
 			type: "SAME_PLACEMENT_TO_TWO_BRACKETS";
+			bracketIds: string[];
 	  }
 	// from one bracket e.g. if 1st goes somewhere and 3rd goes somewhere then 2nd must also go somewhere
 	| {
 			type: "GAP_IN_PLACEMENTS";
+			bracketId: string;
 	  }
 	// if round robin groups size is 4 then it doesn't make sense to have destination for 5
 	| {
 			type: "TOO_MANY_PLACEMENTS";
+			bracketId: string;
 	  }
 	// two brackets can not have the same name
 	| {
 			type: "DUPLICATE_BRACKET_NAME";
+			bracketIds: string[];
 	  }
 	// all brackets must have a name that is not an empty string
 	| {
@@ -71,8 +77,92 @@ export type ValidationError =
 			type: "NEGATIVE_PROGRESSION";
 	  };
 
+/** Takes bracket progression as entered by user as input and returns the validated brackets ready for input to the database or errors if any. */
 export function validatedSources(
 	brackets: InputBracket[],
 ): ValidatedBracket[] | ValidationError {
-	return [];
+	let parsed: ValidatedBracket[];
+	try {
+		parsed = toOutputBracketFormat(brackets);
+	} catch (e) {
+		if ((e as { badBracketId: string }).badBracketId) {
+			return {
+				type: "PLACEMENTS_PARSE_ERROR",
+				bracketId: (e as { badBracketId: string }).badBracketId,
+			};
+		}
+
+		throw e;
+	}
+
+	if (!resolvesWinner(parsed)) {
+		return {
+			type: "NOT_RESOLVING_WINNER",
+		};
+	}
+
+	return parsed;
 }
+
+function toOutputBracketFormat(brackets: InputBracket[]): ValidatedBracket[] {
+	const result = brackets.map((bracket) => {
+		const { id, sources, ...rest } = bracket;
+
+		return {
+			...rest,
+			sources: sources?.map((source) => {
+				const placements = parsePlacements(source.placements);
+				if (!placements) {
+					throw { badBracketId: bracket.id };
+				}
+
+				return {
+					bracketIdx: brackets.findIndex((b) => b.id === source.bracketId),
+					placements,
+				};
+			}),
+		};
+	});
+
+	invariant(
+		result.every(
+			(bracket) =>
+				!bracket.sources ||
+				bracket.sources.every((source) => source.bracketIdx >= 0),
+			"Bracket source not found",
+		),
+	);
+
+	return result;
+}
+
+function parsePlacements(placements: string) {
+	const parts = placements.split(",");
+
+	const result: number[] = [];
+
+	for (let part of parts) {
+		part = part.trim();
+		const isValid = part.match(/^\d+(-\d+)?$/);
+		if (!isValid) return null;
+
+		if (part.includes("-")) {
+			const [start, end] = part.split("-").map(Number);
+
+			for (let i = start; i <= end; i++) {
+				result.push(i);
+			}
+		} else {
+			result.push(Number(part));
+		}
+	}
+
+	return result;
+}
+
+function resolvesWinner(brackets: ValidatedBracket[]) {
+	return true;
+}
+
+// xxx: tests & export?
+function resolveFinals() {}
--- app/features/tournament-bracket/core/Progression.test.ts
+++ app/features/tournament-bracket/core/Progression.test.ts
@@ -9,12 +9,142 @@
 // - many starting brackets https://discord.com/channels/299182152161951744/1288567178836312194/1288567178836312194
 // - startTime???
 
-import { it } from "vitest";
+import { describe, expect, it } from "vitest";
+import * as Progression from "./Progression.server";
 
 // approximate number of teams
 
 // standings to new bracket participants
 
-it("placeholder", () => {
-	// TODO
+describe("validatedSources - PLACEMENTS_PARSE_ERROR", () => {
+	const getValidatedBracketsFromPlacements = (placements: string) => {
+		return Progression.validatedSources([
+			{ id: "1", name: "Bracket 1", type: "round_robin", settings: {} },
+			{
+				id: "2",
+				name: "Bracket 2",
+				type: "single_elimination",
+				settings: {},
+				sources: [
+					{
+						bracketId: "1",
+						placements,
+					},
+				],
+			},
+		]);
+	};
+
+	it("parses placements correctly (separated by comma)", () => {
+		const result = getValidatedBracketsFromPlacements(
+			"1,2,3,4",
+		) as Progression.ValidatedBracket[];
+
+		expect(result[1].sources).toEqual([
+			{ bracketIdx: 0, placements: [1, 2, 3, 4] },
+		]);
+	});
+
+	it("parses placements correctly (separated by line)", () => {
+		const result = getValidatedBracketsFromPlacements(
+			"1-4",
+		) as Progression.ValidatedBracket[];
+
+		expect(result[1].sources).toEqual([
+			{ bracketIdx: 0, placements: [1, 2, 3, 4] },
+		]);
+	});
+
+	it("parses placements correctly (separated by a mix)", () => {
+		const result = getValidatedBracketsFromPlacements(
+			"1,2,3-4",
+		) as Progression.ValidatedBracket[];
+
+		expect(result[1].sources).toEqual([
+			{ bracketIdx: 0, placements: [1, 2, 3, 4] },
+		]);
+	});
+
+	it("handles placement where ranges start and end is the same", () => {
+		const result = getValidatedBracketsFromPlacements(
+			"1-1",
+		) as Progression.ValidatedBracket[];
+
+		expect(result[1].sources).toEqual([{ bracketIdx: 0, placements: [1] }]);
+	});
+
+	it("handles parsing with extra white space", () => {
+		const result = getValidatedBracketsFromPlacements(
+			"1, 2, 3,4 ",
+		) as Progression.ValidatedBracket[];
+
+		expect(result[1].sources).toEqual([
+			{ bracketIdx: 0, placements: [1, 2, 3, 4] },
+		]);
+	});
+
+	it("parsing fails if invalid characters", () => {
+		const error = getValidatedBracketsFromPlacements(
+			"1st,2nd,3rd,4th",
+		) as Progression.ValidationError;
+
+		expect(error.type).toBe("PLACEMENTS_PARSE_ERROR");
+	});
 });
+
+const getValidatedBrackets = (
+	brackets: Omit<Progression.InputBracket, "id" | "name">[],
+) =>
+	Progression.validatedSources(
+		brackets.map((b, i) => ({ id: String(i), name: `Bracket ${i + 1}`, ...b })),
+	);
+
+describe("validatedSources - other rules", () => {
+	it("handles NOT_RESOLVING_WINNER (only round robin)", () => {
+		const error = getValidatedBrackets([
+			{
+				settings: {},
+				type: "round_robin",
+			},
+		]) as Progression.ValidationError;
+
+		expect(error.type).toBe("NOT_RESOLVING_WINNER");
+	});
+
+	it("handles NOT_RESOLVING_WINNER (ends in round robin)", () => {
+		const error = getValidatedBrackets([
+			{
+				settings: {},
+				type: "single_elimination",
+			},
+			{
+				settings: {},
+				type: "round_robin",
+				sources: [
+					{
+						bracketId: "0",
+						placements: "1,2",
+					},
+				],
+			},
+		]) as Progression.ValidationError;
+
+		expect(error.type).toBe("NOT_RESOLVING_WINNER");
+	});
+
+	it("handles NOT_RESOLVING_WINNER (swiss with many groups)", () => {
+		const error = getValidatedBrackets([
+			{
+				settings: {
+					groupCount: 2,
+				},
+				type: "swiss",
+			},
+		]) as Progression.ValidationError;
+
+		expect(error.type).toBe("NOT_RESOLVING_WINNER");
+	});
+});
+
+// separate tests for known valid rules
+// - swiss, 1 group
