69d390281c7041ac91825f8a3c5b7b8813e32df9
Ameowzin <sebastian.yen@gmail.com>
2025-01-14 20:01:47 -0800

    Added Cat Food Bowl badges

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -15,6 +15,11 @@ export const homemadeBadges: BadgeInfo[] = [
 	// 	authorDiscordId: "123456789012345678",
 	// },
 	{
+		displayName: "Dr. Meowser's Cat Food Bowl",
+		fileName: "catfoodbowl",
+		authorDiscordId: "324396128906903552",
+	},
+	{
 		displayName: "Squids For Palestinian Kids",
 		fileName: "three_course_pal",
 		authorDiscordId: "338806780446638082",
