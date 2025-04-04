4593c956a578a4451ce96a718f285bc9ab7e4147
ItIsSkitty <tomtucker9114@gmail.com>
2025-04-04 00:11:56 -0400

    add ranked refreshments badge

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -919,4 +919,19 @@ export const homemadeBadges: BadgeInfo[] = [
 		fileName: "artsuppliesbronze",
 		authorDiscordId: "1320944066002681876",
 	},
+	{
+		displayName: "Ranked Refreshments: Lemon",
+		fileName: "ranked_refreshments_x_3",
+		authorDiscordId: "336683473714544641",
+	},
+	{
+		displayName: "Ranked Refreshments: Lime",
+		fileName: "ranked_refreshments_4_7",
+		authorDiscordId: "336683473714544641",
+	},
+	{
+		displayName: "Ranked Refreshments: Dragonfruit",
+		fileName: "ranked_refreshments_8_11",
+		authorDiscordId: "336683473714544641",
+	},
 ];
