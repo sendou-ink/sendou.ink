9584f78c96f1bddd79233a213e9041c973832644
Taysonbucket <hyperplex.gg@gmail.com>
2025-04-16 11:54:28 -0500

    Update homemade.ts file

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -999,4 +999,9 @@ export const homemadeBadges: BadgeInfo[] = [
 		fileName: "dynamo",
 		authorDiscordId: "534502084134043648",
 	},
+	{
+		displayName: "Takoroka Cup",
+		fileName: "takorokacup",
+		authorDiscordId: "1320944066002681876",
+	},
 ];
