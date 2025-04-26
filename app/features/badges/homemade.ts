7d623e37c8962c73fafbbbe2beaf0df58ef1c746
Taysonbucket <hyperplex.gg@gmail.com>
2025-04-26 09:23:44 -0500

    Update homemade.ts file

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -1099,4 +1099,9 @@ export const homemadeBadges: BadgeInfo[] = [
 		fileName: "serenade",
 		authorDiscordId: "617399529532293150",
 	},
+	{
+		displayName: "Nether Portal Achievement",
+		fileName: "netherportal",
+		authorDiscordId: "1320944066002681876",
+	},
 ];
