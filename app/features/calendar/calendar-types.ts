5b32d56f6838f57ed94346c41d3fbc975e680a24
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-05-01 13:51:57 +0300

    Add types

--- app/features/calendar/calendar-types.ts
+++ app/features/calendar/calendar-types.ts
@@ -1,4 +1,22 @@
-export interface FollowUpBracket {
-	name: string;
-	placements: Array<number>;
+import type { CalendarEventTag, Tables } from "~/db/tables";
+import type { ShowcaseTournament } from "../front-page/core/ShowcaseTournaments.server";
+
+export interface CalendarEvent
+	extends Omit<
+		ShowcaseTournament,
+		"id" | "startTime" | "firstPlacer" | "isRanked"
+	> {
+	tags: Array<CalendarEventTag>;
+	url: string;
+	/** Is the tournament ranked? If null, tournament is not hosted on sendou.ink */
+	isRanked: boolean | null;
+	badges: Array<Pick<Tables["Badge"], "code" | "displayName" | "hue">>;
+}
+
+export interface GroupedCalendarEvents {
+	date: Date;
+	events: {
+		shown: CalendarEvent[];
+		hidden: CalendarEvent[];
+	};
 }
