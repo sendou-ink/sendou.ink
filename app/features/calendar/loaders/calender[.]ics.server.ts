aa046631c12e1be96fcc05ae2180477eeb314969
kytoaa <58483576+kytoaa@users.noreply.github.com>
2025-05-04 22:49:43 +0100

    working ical file generation, needs more checks like 75 octet line limit

--- app/features/calendar/loaders/calender[.]ics.server.ts
+++ app/features/calendar/loaders/calender[.]ics.server.ts
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import type { PersistedCalendarEventTag } from "~/db/tables";
 import * as CalendarRepository from "../CalendarRepository.server";
+import { databaseTimestampToDate } from "~/utils/dates";
 import {
   loaderFilterSearchParamsSchema,
   loaderTournamentsOnlySearchParamsSchema,
@@ -41,10 +42,62 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   });
 
   // TODO: convert event list to ical
-  return new Response(JSON.stringify(await events), {
+  const iCalFeed = eventsAsICal(await events);
+  return new Response(iCalFeed, {
     status: 200,
     headers: {
       "Content-Type": "text/calendar",
     },
   });
 };
+
+function eventsAsICal(
+  events: CalendarRepository.FindAllBetweenTwoTimestampsItem[]
+): Buffer {
+  const properties =
+    "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//sendou.ink//calendar//EN\r\n";
+
+  const data =
+    events.reduce(
+      (acc, event) => acc + eventInfoAsICalEvent(event),
+      properties
+    ) + "END:VCALENDAR\r\n";
+
+  return Buffer.from(data, "utf8");
+}
+
+function eventInfoAsICalEvent(
+  event: CalendarRepository.FindAllBetweenTwoTimestampsItem
+): string {
+  const date = new Date();
+  const eventDate = databaseTimestampToDate(event.startTime);
+  const eventEndDate = new Date(databaseTimestampToDate(event.startTime));
+  eventEndDate.setHours(eventEndDate.getHours() + 3); // arbitrary length of 3 hours, maybe change
+  const eventLink = `https://sendou.ink/calendar/${event.eventId}`;
+  const tags = event.tags.reduce((acc, tag) => `${acc},${tag}`, "").slice(1);
+
+  return (
+    "BEGIN:VEVENT\r\n" +
+    `UID:${crypto.randomUUID()}\r\n` +
+    `DTSTAMP:${dateAsICalDate(date)}\r\n` +
+    `DTSTART:${dateAsICalDate(eventDate)}\r\n` +
+    `DTEND:${dateAsICalDate(eventEndDate)}\r\n` +
+    `SUMMARY:${event.name}\r\n` +
+    `LOCATION:${eventLink}\r\n` +
+    `ORGANISER:${event.username}\r\n` +
+    `CATEGORIES:${tags}\r\n` +
+    `END:VEVENT\r\n`
+  );
+}
+
+function dateAsICalDate(date: Date): string {
+  return `${date.getUTCFullYear()}${(date.getUTCMonth() + 1)
+    .toString()
+    .padStart(2, "0")}${date.getUTCDate().toString().padStart(2, "0")}T${date
+    .getUTCHours()
+    .toString()
+    .padStart(2, "0")}${date.getUTCMinutes().toString().padStart(2, "0")}${date
+    .getUTCSeconds()
+    .toString()
+    .padStart(2, "0")}Z`;
+}
