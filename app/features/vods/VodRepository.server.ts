b597b4742fb8e51532245af2065c7c28aad0197b
Phil <phil.mey.06@gmail.com>
2025-06-25 09:48:24 +0200

    replace the findVods sql query with a kysely query

--- app/features/vods/VodRepository.server.ts
+++ app/features/vods/VodRepository.server.ts
@@ -1,5 +1,104 @@
+import { sql } from "kysely";
 import { db } from "~/db/sql";
+import type { Tables } from "~/db/tables";
+import type {
+	MainWeaponId,
+	ModeShort,
+	StageId,
+} from "~/modules/in-game-lists/types";
+import { weaponIdToArrayWithAlts } from "~/modules/in-game-lists/weapon-ids";
+import { VODS_PAGE_BATCH_SIZE } from "./vods-constants";
+import type { ListVod } from "./vods-types";
 
 export function deleteById(id: number) {
 	return db.deleteFrom("UnvalidatedVideo").where("id", "=", id).execute();
+}
+
+export async function findVods({
+	weapon,
+	mode,
+	stageId,
+	type,
+	userId,
+	limit = VODS_PAGE_BATCH_SIZE,
+}: {
+	weapon?: MainWeaponId;
+	mode?: ModeShort;
+	stageId?: StageId;
+	type?: Tables["Video"]["type"];
+	userId?: number;
+	limit?: number;
+}): Promise<Array<ListVod>> {
+	let query = db
+		.selectFrom("Video")
+		.leftJoin("VideoMatch", "Video.id", "VideoMatch.videoId")
+		.leftJoin(
+			"VideoMatchPlayer",
+			"VideoMatch.id",
+			"VideoMatchPlayer.videoMatchId",
+		)
+		.leftJoin("User", "VideoMatchPlayer.playerUserId", "User.id")
+		.selectAll("Video")
+		.select(({ fn, val, ref }) => [
+			sql<
+				Array<number>
+			>`json_group_array(distinct ${ref("VideoMatchPlayer.weaponSplId")})`.as(
+				"weapons",
+			),
+			fn
+				.agg("json_group_array", ["VideoMatchPlayer.playerName"])
+				.as("playerNames"),
+			fn
+				.agg("json_group_array", [
+					fn.agg("json_object", [
+						val("username"),
+						"User.username",
+						val("discordId"),
+						"User.discordId",
+						val("discordAvatar"),
+						"User.discordAvatar",
+						val("customUrl"),
+						"User.customUrl",
+					]),
+				])
+				.as("players"),
+		]);
+	if (userId) {
+		query = query.where("User.id", "=", userId);
+	} else {
+		if (type) {
+			query = query.where("Video.type", "=", type);
+		}
+		if (mode) {
+			query = query.where("VideoMatch.mode", "=", mode);
+		}
+		if (stageId) {
+			query = query.where("VideoMatch.stageId", "=", stageId);
+		}
+	}
+	if (weapon) {
+		query = query.where((eb) =>
+			eb.or(
+				weaponIdToArrayWithAlts(weapon).map((weapon) => {
+					return eb("VideoMatchPlayer.weaponSplId", "=", weapon);
+				}),
+			),
+		);
+	}
+	const result = query
+		.groupBy("Video.id")
+		.orderBy("Video.youtubeDate", "desc")
+		.limit(limit)
+		.execute();
+
+	const vods = (await result).map((value) => {
+		const { playerNames, players, ...vod } = value;
+		const playerNamesArray: string[] = playerNames as string[];
+		const playersArray: Tables["User"][] = players as Tables["User"][];
+		return {
+			...vod,
+			pov: playerNamesArray[0] ?? playersArray[0],
+		};
+	});
+	return vods as ListVod[];
 }
