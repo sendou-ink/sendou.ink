29220bebf723810eb4f886b6ab81a16b8d939b10
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-06-30 19:21:42 +0300

    Fix Tri-Stringer etc. 3rd kit on Object Damage Calculator by generalizing the logic

--- app/features/build-analyzer/core/utils.ts
+++ app/features/build-analyzer/core/utils.ts
@@ -16,6 +16,7 @@ import {
 	specialWeaponIds,
 	subWeaponIds,
 	weaponCategories,
+	weaponIdToBaseWeaponId,
 } from "~/modules/in-game-lists/weapon-ids";
 import invariant from "~/utils/invariant";
 import type { Unpacked } from "~/utils/types";
@@ -329,3 +330,32 @@ export function damageIsSubWeaponDamage(
 ): damage is Unpacked<AnalyzedBuild["stats"]["subWeaponDefenseDamages"]> {
 	return typeof (damage as SubWeaponDamage).subWeaponId === "number";
 }
+
+const rawMultiShot: Partial<Record<MainWeaponId, number>> = {
+	// L-3
+	300: 3,
+	// H-3
+	310: 3,
+	// Tri-Stringer,
+	7010: 3,
+	// REEF-LUX,
+	7020: 3,
+	// Wellstring V,
+	7030: 5,
+	// Bloblobber
+	3030: 4,
+	// Dread Winger
+	3050: 2,
+};
+
+/**
+ * Returns the multi-shot count for a given weapon ID. Multi-shot refers to the number of projectiles fired in a single shot,
+ * e.g. H-3 Nozzlenose fires 3 projectiles per one trigger press.
+ *
+ * @returns The multi-shot count associated with the weapon, or `undefined` if not found.
+ */
+export const weaponIdToMultiShotCount = (weaponId: MainWeaponId) => {
+	return rawMultiShot[
+		weaponIdToBaseWeaponId(weaponId) as keyof typeof rawMultiShot
+	];
+};
