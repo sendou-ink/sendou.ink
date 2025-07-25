12590ba0dfad60d68b71765802436d7fec2a2aa5
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-07-25 21:48:07 +0300

    Drop some unused columns, make TournamentRounds.maps always to be defined

--- app/features/api-public/routes/tournament-match.$id.ts
+++ app/features/api-public/routes/tournament-match.$id.ts
@@ -49,7 +49,6 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
 				"TournamentMatch.opponentOne",
 				"TournamentMatch.opponentTwo",
 				"Tournament.mapPickingStyle",
-				"TournamentMatch.bestOf",
 				"TournamentRound.maps",
 				jsonArrayFrom(
 					eb
@@ -122,7 +121,6 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
 			: [];
 
 		return resolveMapList({
-			bestOf: match.bestOf,
 			tournamentId: match.tournamentId,
 			matchId: id,
 			teams: [match.opponentOne.id, match.opponentTwo.id],
