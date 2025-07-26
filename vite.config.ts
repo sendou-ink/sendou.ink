4f5b7bf0740adf4ebf53ea9c665a1764a3685347
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-07-26 08:20:39 +0300

    Opens a test page

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,34 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import babel from "vite-plugin-babel";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { configDefaults } from "vitest/config";
 
-installGlobals();
-
-const ReactCompilerConfig = {
-	target: "18",
-};
-
-export default defineConfig(() => {
-	return {
-		ssr: {
-			noExternal: ["react-charts", "react-use"],
-		},
-		plugins: [
-			remix({
-				ignoredRouteFiles: ["**/.*", "**/*.json", "**/components/*"],
-				serverModuleFormat: "esm",
-				future: {
-					v3_fetcherPersist: true,
-					v3_relativeSplatPath: true,
-					v3_throwAbortReason: true,
-					v3_routeConfig: true,
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		projects: [
+			{
+				extends: "./vite.config.ts",
+				test: {
+					name: "client",
+					environment: "browser",
+					browser: {
+						enabled: true,
+						provider: "playwright",
+						instances: [{ browser: "chromium" }],
+					},
+					include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+					exclude: ["src/lib/server/**"],
+					setupFiles: ["./vitest-setup-client.ts"],
 				},
-			}),
-			babel({
-				filter: /\.[jt]sx?$/,
-				babelConfig: {
-					presets: ["@babel/preset-typescript"],
-					plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+			},
+			{
+				extends: "./vite.config.ts",
+				test: {
+					name: "server",
+					environment: "node",
+					include: ["src/**/*.{test,spec}.{js,ts}"],
+					exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"],
 				},
-			}),
-			tsconfigPaths(),
+			},
 		],
-		test: {
-			exclude: [...configDefaults.exclude, "e2e/**"],
-		},
-		build: {
-			// this is mostly done so that i18n jsons as defined in ./app/modules/i18n/loader.ts
-			// do not end up in the js bundle as minimized strings
-			// if we decide later that this is a useful optimization in some cases then we can
-			// switch the value to a callback one that checks the file path
-			assetsInlineLimit: 0,
-		},
-	};
+	},
 });
