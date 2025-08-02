af5e4d006b24a68b59a362589b9679a3ea346cbd
soap <david.parada.w@gmail.com>
2025-08-02 02:15:30 -0700

    Add player country info to teams in tourney API (#2478)

--- app/features/api-public/schema.ts
+++ app/features/api-public/schema.ts
@@ -171,6 +171,10 @@ export type GetTournamentTeamsResponse = Array<{
 		 * @example "https://cdn.discordapp.com/avatars/79237403620945920/6fc41a44b069a0d2152ac06d1e496c6c.png"
 		 */
 		avatarUrl: string | null;
+		/**
+		 * @example "FI"
+		 */
+		country: string | null;
 		captain: boolean;
 		/**
 		 * Splatoon 3 splashtag name & ID. Notice the value returned is the player's set name at the time of the tournament.
