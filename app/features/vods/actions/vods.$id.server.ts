9113e4c4d8e413b21480647c9c4f162a2c218617
Phil <phil.mey.06@gmail.com>
2025-08-11 17:45:25 +0200

    change imports to preferred style

--- app/features/vods/actions/vods.$id.server.ts
+++ app/features/vods/actions/vods.$id.server.ts
@@ -3,13 +3,12 @@ import { requireUser } from "~/features/auth/core/user.server";
 import { badRequestIfFalsy, unauthorizedIfFalsy } from "~/utils/remix.server";
 import { userVodsPage } from "~/utils/urls";
 import * as VodRepository from "../VodRepository.server";
-import { findVodById } from "../VodRepository.server";
 import { canEditVideo } from "../vods-utils";
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
 	const user = await requireUser(request);
 
-	const vod = badRequestIfFalsy(await findVodById(Number(params.id)));
+	const vod = badRequestIfFalsy(await VodRepository.findVodById(Number(params.id)));
 
 	unauthorizedIfFalsy(
 		canEditVideo({
