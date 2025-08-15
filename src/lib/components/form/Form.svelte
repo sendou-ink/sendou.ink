8619f16c681a110cddf0b8c8f344022c26bbc5cc
hfcRed <hfcred@gmx.net>
2025-08-15 17:49:00 +0200

    Enhance + cleanup

--- src/lib/components/form/Form.svelte
+++ src/lib/components/form/Form.svelte
@@ -1,17 +1,17 @@
 <script lang="ts" generics="T extends z.ZodType<object>">
 	import type { RemoteForm } from '@sveltejs/kit';
 	import type { Snippet } from 'svelte';
-	import { setContext, tick } from 'svelte';
+	import { tick } from 'svelte';
 	import { m } from '$lib/paraglide/messages';
 	import { zodErrorsToFormErrors } from '$lib/utils/zod';
+	import { formContext } from './context';
 	import z from 'zod';
 	import Button from '../buttons/Button.svelte';
-	import { formContext } from './context';
 
 	type Output = z.output<T>;
 
 	interface Props {
-		children: Snippet<[T]>; // Pass schema type to children
+		children: Snippet;
 		heading?: string;
 		action: RemoteForm<void | { errors: Partial<Record<keyof Output, string>> }>;
 		schema: T;
@@ -22,48 +22,53 @@
 	const id = $props.id();
 
 	let form: HTMLFormElement;
-
 	let errors = $state<Partial<Record<keyof Output, string>>>({});
-
-	$effect(() => {
-		errors = action.result?.errors ?? {};
-		tick().then(() => focusFirstInvalidField());
-	});
-
-	function handleBlur() {
-		const formData = new FormData(form);
-		const data = Object.fromEntries(formData.entries());
-		const parsed = z.safeParse(schema, data);
-
-		if (parsed.success) {
-			errors = {};
-			return;
-		}
-
-		errors = zodErrorsToFormErrors(parsed.error);
-	}
-
-	function focusFirstInvalidField() {
-		const invalidElement = form.querySelector('[aria-invalid="true"]') as HTMLInputElement;
-
-		invalidElement?.focus();
-		invalidElement?.scrollIntoView({ behavior: 'smooth', block: 'center' });
-	}
 
 	formContext.set({
 		schema,
 		defaultValues,
 		errors: () => errors,
-		onblur: handleBlur
+		onblur: () => validateForm(new FormData(form))
 	});
+
+	function validateForm(data: FormData) {
+		const parsed = z.safeParse(schema, Object.fromEntries(data.entries()));
+		if (parsed.success) {
+			errors = {};
+			return true;
+		}
+
+		errors = zodErrorsToFormErrors(parsed.error);
+		return false;
+	}
+
+	function focusFirstInvalid() {
+		tick().then(() => {
+			const invalidElement = form.querySelector('[aria-invalid="true"]') as HTMLInputElement;
+
+			invalidElement?.focus();
+			invalidElement?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+		});
+	}
+
+	async function enhanced({ data, submit }: { data: FormData; submit: () => Promise<void> }) {
+		if (!validateForm(data)) {
+			focusFirstInvalid();
+			return;
+		}
+
+		await submit();
+		errors = action.result?.errors ?? {};
+		focusFirstInvalid();
+	}
 </script>
 
-<form {id} bind:this={form} {...action} class="stack md-plus items-start">
+<form {id} bind:this={form} {...action.enhance(enhanced)} class="stack md-plus items-start">
 	{#if heading}
 		<h1 class="text-lg">{heading}</h1>
 	{/if}
 
-	{@render children(schema)}
+	{@render children()}
 
 	<div class="stack horizontal lg justify-between mt-6 w-full">
 		<Button type="submit" loading={action.pending > 0} disabled={Object.keys(errors).length > 0}>
