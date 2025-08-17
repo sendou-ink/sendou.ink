7394591d87a26bfd10e0d2206812230430c03028
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-08-17 16:48:17 +0300

    Update casings

---
+++ src/lib/components/Flag.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { getLocale } from '$lib/paraglide/runtime';
+	import { countryCodeToTranslatedName } from '$lib/utils/i18n';
+
+	interface Props {
+		countryCode: string;
+		tiny?: boolean;
+	}
+
+	let { countryCode, tiny = false }: Props = $props();
+
+	const title = $derived(
+		countryCodeToTranslatedName({
+			countryCode,
+			language: getLocale()
+		})
+	);
+</script>
+
+<div
+	class={[
+		'twf',
+		`twf-${countryCode.toLowerCase()}`,
+		{
+			'twf-s': tiny
+		}
+	]}
+	data-testid={`flag-${countryCode}`}
+	{title}
+></div>
---
+++ src/lib/components/Markdown.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	interface Props {
+		/** Parsed markdown content. NOTE: this needs to be sanitized. */
+		content: string;
+	}
+
+	let { content }: Props = $props();
+</script>
+
+<div class="markdown-content">
+	<!-- eslint-disable svelte/no-at-html-tags -->
+	{@html content}
+</div>
+
+<style>
+	.markdown-content :global {
+		a {
+			font-weight: var(--semi-bold);
+			color: var(--theme);
+		}
+
+		h1 {
+			padding-block-end: var(--s-6);
+		}
+
+		p {
+			padding-block: var(--s-2-5);
+		}
+	}
+</style>
