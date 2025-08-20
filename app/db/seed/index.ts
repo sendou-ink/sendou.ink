5aa10ccdfdb503261bdef8ba5d2b8fe7f1463373
Phil <phil.mey.06@gmail.com>
2025-08-20 23:30:21 +0200

    add missing async await

--- app/db/seed/index.ts
+++ app/db/seed/index.ts
@@ -1722,8 +1722,8 @@ function otherTeams() {
 	}
 }
 
-function realVideo() {
-	VodRepository.createVod({
+async function realVideo() {
+	await VodRepository.createVod({
 		type: "TOURNAMENT",
 		youtubeUrl: youtubeIdToYoutubeUrl("M4aV-BQWlVg"),
 		date: { day: 2, month: 2, year: 2023 },
@@ -1776,8 +1776,8 @@ function realVideo() {
 	});
 }
 
-function realVideoCast() {
-	VodRepository.createVod({
+async function realVideoCast() {
+	await VodRepository.createVod({
 		type: "CAST",
 		youtubeUrl: youtubeIdToYoutubeUrl("M4aV-BQWlVg"),
 		date: { day: 2, month: 2, year: 2023 },
