1a5226ab9826b4990eb30e6dceca2176429f0d69
hfcRed <hfcred@gmx.net>
2025-08-31 19:41:51 +0200

    Add context docs

--- src/lib/runes/context.ts
+++ src/lib/runes/context.ts
@@ -2,23 +2,56 @@
 
 import { getContext, hasContext, setContext } from 'svelte';
 
+/**
+ * Creates a type-safe context.
+ *
+ * @template TContext The type of the context value.
+ */
 export class Context<TContext> {
 	readonly #name: string;
 	readonly #key: symbol;
 
+	/**
+	 * Creates a type-safe context.
+	 *
+	 * @param name The name of the context.
+	 *
+	 * @example
+	 * ```ts
+	 * // Create context in a separate file
+	 * export const theme = new Context<'light' | 'dark'>('theme');
+	 *
+	 * // Set context in a component
+	 * theme.set('light');
+	 *
+	 * // Read context in a child component
+	 * const theme = theme.get();
+	 * ```
+	 */
 	constructor(name: string) {
 		this.#name = name;
 		this.#key = Symbol(name);
 	}
 
+	/**
+	 * The key used to get and set the context value.
+	 */
 	get key(): symbol {
 		return this.#key;
 	}
 
+	/**
+	 * Returns true if the context value has been set by a parent.
+	 */
 	exists(): boolean {
 		return hasContext(this.#key);
 	}
 
+	/**
+	 * Gets the context value of the closest parent.
+	 *
+	 * @throws Error if the context does not exist.
+	 */
 	get(): TContext {
 		const context: TContext | undefined = getContext(this.#key);
 		if (context === undefined) {
@@ -27,6 +60,9 @@ export class Context<TContext> {
 		return context;
 	}
 
+	/**
+	 * Gets the context value of the closest parent or the provided fallback value if the context does not exist.
+	 */
 	getOr<TFallback>(fallback: TFallback): TContext | TFallback {
 		const context: TContext | undefined = getContext(this.#key);
 		if (context === undefined) {
@@ -35,6 +71,9 @@ export class Context<TContext> {
 		return context;
 	}
 
+	/**
+	 * Sets the context value for this key and returns it.
+	 */
 	set(context: TContext): TContext {
 		return setContext(this.#key, context);
 	}
