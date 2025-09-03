40eb7166dc8998e6a0d07df8b057bc25cd6f478e
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-09-03 20:15:36 +0300

    Fixes

--- app/routes.ts
+++ app/routes.ts
@@ -286,6 +286,7 @@ export default [
 			"features/api-public/routes/tournament-match.$id.ts",
 		),
 		route("/org/:id", "features/api-public/routes/org.$id.ts"),
+		route("/team/:id", "features/api-public/routes/team.$id.ts"),
 	]),
 
 	route("/short/:customUrl", "features/user-page/routes/short.$customUrl.ts"),
