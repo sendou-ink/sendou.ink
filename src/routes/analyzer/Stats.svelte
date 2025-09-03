a4a6b229ad58e61c7f45d3dcf256ac3c0c93255a
hfcRed <hfcred@gmx.net>
2025-09-03 19:03:46 +0200

    Add stats component

---
+++ src/routes/analyzer/Stats.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		title: string;
+		bottomText?: string;
+		titleContent?: {
+			text: string;
+			image?: string;
+		};
+		children: Snippet;
+	}
+
+	let { title, titleContent, bottomText, children }: Props = $props();
+</script>
+
+<details>
+	<summary>
+		<span>{title}</span>
+		<div>
+			{#if titleContent?.image}
+				<img src={titleContent?.image} alt={titleContent?.text} />
+			{/if}
+			{#if titleContent?.text}
+				<span>{titleContent?.text}</span>
+			{/if}
+		</div>
+	</summary>
+	<div>
+		{@render children()}
+	</div>
+	{#if bottomText}
+		<p>{bottomText}</p>
+	{/if}
+</details>
