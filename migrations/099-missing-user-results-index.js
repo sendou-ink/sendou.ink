5410ce6a597df9ae09b174bf86cac1c9726bf67d
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-10-15 20:49:51 +0300

    remove if not exists

--- migrations/099-missing-user-results-index.js
+++ migrations/099-missing-user-results-index.js
@@ -2,7 +2,7 @@ export function up(db) {
 	db.transaction(() => {
 		db.prepare(
 			/* sql */ `
-        create index if not exists calendar_event_date_event_id_start_time on "CalendarEventDate"("eventId", "startTime" desc)
+        create index calendar_event_date_event_id_start_time on "CalendarEventDate"("eventId", "startTime" desc)
       `,
 		).run();
 	})();
