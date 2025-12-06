f2db667c3cd69619d8010ea2fbf83666534340fb
Kalle <38327916+Sendouc@users.noreply.github.com>
2025-12-06 12:37:06 +0200

    Delete unused code

--- app/modules/brackets-manager/create.ts
+++ app/modules/brackets-manager/create.ts
@@ -33,9 +33,7 @@ export class Create {
 	private storage: Storage;
 	private stage: InputStage;
 	private readonly seedOrdering: SeedOrdering[];
-	private updateMode: boolean;
 	private enableByesInUpdate: boolean;
-	private currentStageId!: number;
 
 	/**
 	 * Creates an instance of Create, which will handle the creation of the stage.
@@ -48,7 +46,6 @@ export class Create {
 		this.stage = stage;
 		this.stage.settings = this.stage.settings || {};
 		this.seedOrdering = this.stage.settings.seedOrdering || [];
-		this.updateMode = false;
 		this.enableByesInUpdate = false;
 
 		if (!this.stage.name) throw Error("You must provide a name for the stage.");
@@ -94,18 +91,6 @@ export class Create {
 		this.ensureSeedOrdering(stage.id);
 
 		return stage;
-	}
-
-	/**
-	 * Enables the update mode.
-	 *
-	 * @param stageId ID of the stage.
-	 * @param enableByes Whether to use BYEs or TBDs for `null` values in an input seeding.
-	 */
-	public setExisting(stageId: number, enableByes: boolean): void {
-		this.updateMode = true;
-		this.currentStageId = stageId;
-		this.enableByesInUpdate = enableByes;
 	}
 
 	/**
@@ -427,33 +412,17 @@ export class Create {
 		)
 			return;
 
-		let existing: Match | null = null;
-		let status = helpers.getMatchStatus(opponents);
-
-		if (this.updateMode) {
-			existing = this.storage.selectFirst("match", {
-				round_id: roundId,
-				number: matchNumber,
-			});
-
-			if (existing) {
-				// Keep the most advanced status when updating a match.
-				const existingStatus = helpers.getMatchStatus(existing);
-				if (existingStatus > status) status = existingStatus;
-			}
-		}
-
 		const parentId = this.insertMatch(
 			{
 				number: matchNumber,
 				stage_id: stageId,
 				group_id: groupId,
 				round_id: roundId,
-				status: status,
+				status: helpers.getMatchStatus(opponents),
 				opponent1,
 				opponent2,
 			},
-			existing,
+			null,
 		);
 
 		if (parentId === -1) throw Error("Could not insert the match.");
@@ -755,14 +724,7 @@ export class Create {
 	 * @param stage The stage to insert.
 	 */
 	private insertStage(stage: OmitId<Stage>): number {
-		let existing: Stage | null = null;
-
-		if (this.updateMode)
-			existing = this.storage.select("stage", this.currentStageId);
-
-		if (!existing) return this.storage.insert("stage", stage);
-
-		return existing.id;
+		return this.storage.insert("stage", stage);
 	}
 
 	/**
@@ -771,18 +733,7 @@ export class Create {
 	 * @param group The group to insert.
 	 */
 	private insertGroup(group: OmitId<Group>): number {
-		let existing: Group | null = null;
-
-		if (this.updateMode) {
-			existing = this.storage.selectFirst("group", {
-				stage_id: group.stage_id,
-				number: group.number,
-			});
-		}
-
-		if (!existing) return this.storage.insert("group", group);
-
-		return existing.id;
+		return this.storage.insert("group", group);
 	}
 
 	/**
@@ -791,18 +742,7 @@ export class Create {
 	 * @param round The round to insert.
 	 */
 	private insertRound(round: OmitId<Round>): number {
-		let existing: Round | null = null;
-
-		if (this.updateMode) {
-			existing = this.storage.selectFirst("round", {
-				group_id: round.group_id,
-				number: round.number,
-			});
-		}
-
-		if (!existing) return this.storage.insert("round", round);
-
-		return existing.id;
+		return this.storage.insert("round", round);
 	}
 
 	/**
