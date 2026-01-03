6919d819986635cd4dd53465016dc07844d5fcd2
hfcRed <hfcred@gmx.net>
2026-01-03 10:33:10 +0100

    Remove duplicate type decleration

--- app/features/api-public/schema.ts
+++ app/features/api-public/schema.ts
@@ -444,14 +444,6 @@ type RankTierName =
 	| "BRONZE"
 	| "IRON";
 
-type SubjectPronoun = "he" | "she" | "they" | "it" | "any";
-type ObjectPronoun = "him" | "her" | "them" | "its" | "all";
-
-export interface Pronouns {
-	subject: SubjectPronoun;
-	object: ObjectPronoun;
-}
-
 type Badge = {
 	/**
 	 * @example "Monday Afterparty"
