3805736463caa9cbc4cc9b86c9af56249b09a1d3
hfcRed <hfcred@gmx.net>
2026-01-04 19:29:19 +0100

    Fix browser setup for new stylesheets

--- app/browser-test-setup.ts
+++ app/browser-test-setup.ts
@@ -3,13 +3,11 @@ import { initReactI18next } from "react-i18next";
 import { config } from "~/modules/i18n/config";
 import { resources } from "~/modules/i18n/resources.browser";
 
-import "~/styles/common.css";
-import "~/styles/elements.css";
-import "~/styles/flags.css";
-import "~/styles/layout.css";
-import "~/styles/reset.css";
-import "~/styles/utils.css";
 import "~/styles/vars.css";
+import "~/styles/normalize.css";
+import "~/styles/common.css";
+import "~/styles/utils.css";
+import "~/styles/flags.css";
 
 i18next.use(initReactI18next).init({
 	...config,
