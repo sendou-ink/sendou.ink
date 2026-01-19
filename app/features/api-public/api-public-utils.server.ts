83acfb9571da38ef5d072aa428d1d92dada866b3
Kalle <38327916+Sendouc@users.noreply.github.com>
2026-01-19 20:47:47 +0200

    Remove PUBLIC_API_TOKENS

--- app/features/api-public/api-public-utils.server.ts
+++ app/features/api-public/api-public-utils.server.ts
@@ -2,14 +2,9 @@ import type { ApiTokenType } from "~/db/tables";
 import * as ApiRepository from "~/features/api/ApiRepository.server";
 
 async function loadApiTokensCache() {
-	const envTokens = process.env.PUBLIC_API_TOKENS?.split(",") ?? [];
 	const dbTokens = await ApiRepository.allApiTokens();
 
 	const tokenMap = new Map<string, ApiTokenType>();
-
-	for (const token of envTokens) {
-		tokenMap.set(token, "write");
-	}
 
 	for (const { token, type } of dbTokens) {
 		tokenMap.set(token, type);
