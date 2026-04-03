55a1f75b22f781936ac8861c4ee78bd97788b70c
Kalle <38327916+Sendouc@users.noreply.github.com>
2026-04-03 15:21:27 +0300

    Bottom row mocked

--- app/components/match-page/MatchBannerBottomRow.tsx
+++ app/components/match-page/MatchBannerBottomRow.tsx
@@ -1,16 +1,63 @@
 import type { ModeShort } from "~/modules/in-game-lists/types";
 import type { CommonUser } from "~/utils/kysely.server";
+import { Avatar } from "../Avatar";
+import { ModeImage } from "../Image";
 import styles from "./MatchBannerBottomRow.module.css";
 
-export function MatchBannerBottomRow({
-	games,
-	activeRosters,
-}: {
+interface MatchBannerBottomRowProps {
 	games: Array<{ mode: ModeShort; winner: "ALPHA" | "BRAVO" }>;
 	activeRosters: {
 		alpha: CommonUser[];
 		bravo: CommonUser[];
 	};
-}) {
-	return <div className={styles.root} />;
+}
+
+export function MatchBannerBottomRow({
+	games,
+	activeRosters,
+}: MatchBannerBottomRowProps) {
+	return (
+		<div className={styles.root}>
+			<ModeProgress games={games} />
+			<ActiveRosters activeRosters={activeRosters} />
+		</div>
+	);
+}
+
+function ModeProgress({ games }: Pick<MatchBannerBottomRowProps, "games">) {
+	return (
+		<div className={styles.modeProgress}>
+			<div className={styles.mode}>
+				<ModeImage mode="SZ" size={12} />
+			</div>
+			<div className={styles.mode}>
+				<ModeImage mode="TC" size={12} />
+			</div>
+			<div className={styles.mode}>
+				<ModeImage mode="RM" size={12} />
+			</div>
+		</div>
+	);
+}
+
+function ActiveRosters({
+	activeRosters,
+}: Pick<MatchBannerBottomRowProps, "activeRosters">) {
+	return (
+		<div className={styles.activeRosters}>
+			<div className={styles.team}>
+				<Avatar user={{ discordAvatar: null, discordId: "asd" }} size="xxs" />
+				<Avatar user={{ discordAvatar: null, discordId: "cx" }} size="xxs" />
+				<Avatar user={{ discordAvatar: null, discordId: "zczxc" }} size="xxs" />
+				<Avatar user={{ discordAvatar: null, discordId: "sgd" }} size="xxs" />
+			</div>
+			<div className={styles.vs}>vs.</div>
+			<div className={styles.team}>
+				<Avatar user={{ discordAvatar: null, discordId: "123" }} size="xxs" />
+				<Avatar user={{ discordAvatar: null, discordId: "2345" }} size="xxs" />
+				<Avatar user={{ discordAvatar: null, discordId: "2351" }} size="xxs" />
+				<Avatar user={{ discordAvatar: null, discordId: "12123" }} size="xxs" />
+			</div>
+		</div>
+	);
 }
