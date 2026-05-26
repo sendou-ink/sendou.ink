3a57ffed23c2615423814c0b3f4ec97768ff7992
Kalle <38327916+Sendouc@users.noreply.github.com>
2026-05-26 20:58:41 +0300

    Tables

---
+++ migrations/145-user-weapon-pool-table.js
@@ -0,0 +1,42 @@
+export function up(db) {
+	db.transaction(() => {
+		db.prepare(
+			/* sql */ `
+				create table "UserWeaponPool" (
+					"userId" integer not null,
+					"sortOrder" integer not null,
+					"weaponSplId" integer not null,
+					"isFavorite" integer not null default 0,
+					foreign key ("userId") references "User"("id") on delete cascade,
+					primary key ("userId", "sortOrder")
+				) strict
+			`,
+		).run();
+
+		db.prepare(
+			/* sql */ `
+				insert into "UserWeaponPool" ("userId", "sortOrder", "weaponSplId", "isFavorite")
+				select
+					u."id",
+					je."key",
+					json_extract(je."value", '$.weaponSplId'),
+					coalesce(json_extract(je."value", '$.isFavorite'), 0)
+				from "User" u, json_each(u."weaponPool") je
+				where u."weaponPool" is not null
+			`,
+		).run();
+
+		db.prepare(
+			/* sql */ `
+				create table "TenStarWeapon" (
+					"userId" integer not null,
+					"weaponSplId" integer not null,
+					foreign key ("userId") references "User"("id") on delete cascade,
+					primary key ("userId", "weaponSplId")
+				) strict
+			`,
+		).run();
+
+		db.pragma("foreign_key_check");
+	})();
+}
