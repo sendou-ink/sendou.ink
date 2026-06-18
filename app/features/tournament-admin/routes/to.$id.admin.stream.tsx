744d7c0e1896b8986131988264757d4073c319bb
Kalle <38327916+Sendouc@users.noreply.github.com>
2026-06-18 13:40:11 +0300

    Hide elements on admin page after tournament is finalized

--- app/features/tournament-admin/routes/to.$id.admin.stream.tsx
+++ app/features/tournament-admin/routes/to.$id.admin.stream.tsx
@@ -1,11 +1,17 @@
+import { Redirect } from "~/components/Redirect";
 import { useTournament } from "~/features/tournament/routes/to.$id";
 import { SendouForm } from "~/form/SendouForm";
+import { tournamentAdminPage } from "~/utils/urls";
 import { adminStreamFormSchema } from "../tournament-admin-staff-schemas";
 
 export { action } from "../actions/to.$id.admin.stream.server";
 
 export default function TournamentAdminStreamPage() {
 	const tournament = useTournament();
+
+	if (tournament.ctx.isFinalized) {
+		return <Redirect to={tournamentAdminPage(tournament.ctx.id)} />;
+	}
 
 	return (
 		<SendouForm
