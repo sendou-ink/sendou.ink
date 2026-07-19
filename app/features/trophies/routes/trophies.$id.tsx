1f111a952e7e5019823455d152b0c813f2cf9cee
hfcRed <hfcred@gmx.net>
2026-07-19 16:19:20 -0400

    Square renderer for /trophies/{id}

--- app/features/trophies/routes/trophies.$id.tsx
+++ app/features/trophies/routes/trophies.$id.tsx
@@ -24,87 +24,89 @@ export default function TrophyDetailsPage() {
 	const special = parseSpecialTrophyCode(trophy.code);
 
 	return (
-		<div className={styles.trophyDetailsContainer}>
-			<Trophy model={trophy.model} />
-			<div className={clsx(styles.trophyDetails, "scrollbar")}>
-				<div className="stack xxs">
-					<p className={styles.trophyName}>{trophy.name}</p>
-					{trophy.organization ? (
-						<p className={styles.trophyMeta}>
-							{t("trophies:details.organization", {
-								name: trophy.organization.name,
-							})}
-						</p>
-					) : null}
-					{trophy.creator ? (
-						<p className={styles.trophyMeta}>
-							{t("trophies:details.createdBy", {
-								name: trophy.creator.username,
-							})}
-						</p>
-					) : null}
-					{trophy.manager ? (
-						<p className={styles.trophyMeta}>
-							{t("trophies:details.managedBy", {
-								name: trophy.manager.username,
-							})}
-						</p>
-					) : null}
-					{special ? (
-						<p className={styles.trophyMeta}>
-							{special.type === "supporter"
-								? t("trophies:special.supporter.description")
-								: t("trophies:special.xp.description", {
-										value: special.value,
-									})}
-						</p>
-					) : null}
-				</div>
-				{special ? null : (
+		<div className={styles.trophyDetailsWrapper}>
+			<div className={styles.trophyDetailsContainer}>
+				<Trophy model={trophy.model} />
+				<div className={clsx(styles.trophyDetails, "scrollbar")}>
+					<div className="stack xxs">
+						<p className={styles.trophyName}>{trophy.name}</p>
+						{trophy.organization ? (
+							<p className={styles.trophyMeta}>
+								{t("trophies:details.organization", {
+									name: trophy.organization.name,
+								})}
+							</p>
+						) : null}
+						{trophy.creator ? (
+							<p className={styles.trophyMeta}>
+								{t("trophies:details.createdBy", {
+									name: trophy.creator.username,
+								})}
+							</p>
+						) : null}
+						{trophy.manager ? (
+							<p className={styles.trophyMeta}>
+								{t("trophies:details.managedBy", {
+									name: trophy.manager.username,
+								})}
+							</p>
+						) : null}
+						{special ? (
+							<p className={styles.trophyMeta}>
+								{special.type === "supporter"
+									? t("trophies:special.supporter.description")
+									: t("trophies:special.xp.description", {
+											value: special.value,
+										})}
+							</p>
+						) : null}
+					</div>
+					{special ? null : (
+						<div className="stack xs">
+							<Divider className={styles.divider} smallText>
+								{t("trophies:details.tournamentHistory")}
+							</Divider>
+							{tournaments.length > 0 ? (
+								<ul className={styles.tournamentHistory}>
+									{tournaments.map((tournament) => (
+										<TournamentHistoryEntry
+											key={tournament.tournamentId}
+											tournament={tournament}
+										/>
+									))}
+								</ul>
+							) : (
+								<p className="text-center text-lighter text-xxs">
+									{t("trophies:details.noTournamentHistory")}
+								</p>
+							)}
+						</div>
+					)}
 					<div className="stack xs">
 						<Divider className={styles.divider} smallText>
-							{t("trophies:details.tournamentHistory")}
+							{t("trophies:details.owners")}
 						</Divider>
-						{tournaments.length > 0 ? (
-							<ul className={styles.tournamentHistory}>
-								{tournaments.map((tournament) => (
-									<TournamentHistoryEntry
-										key={tournament.tournamentId}
-										tournament={tournament}
-									/>
+						{trophy.owners.length > 0 ? (
+							<ul className={styles.owners}>
+								{trophy.owners.map((owner) => (
+									<li key={owner.id}>
+										<Link to={userPage(owner)}>{owner.username}</Link>
+										<span
+											className={clsx(styles.ownerCount, {
+												hidden: owner.count <= 1,
+											})}
+										>
+											×{owner.count}
+										</span>
+									</li>
 								))}
 							</ul>
 						) : (
 							<p className="text-center text-lighter text-xxs">
-								{t("trophies:details.noTournamentHistory")}
+								{t("trophies:details.noOwners")}
 							</p>
 						)}
 					</div>
-				)}
-				<div className="stack xs">
-					<Divider className={styles.divider} smallText>
-						{t("trophies:details.owners")}
-					</Divider>
-					{trophy.owners.length > 0 ? (
-						<ul className={styles.owners}>
-							{trophy.owners.map((owner) => (
-								<li key={owner.id}>
-									<Link to={userPage(owner)}>{owner.username}</Link>
-									<span
-										className={clsx(styles.ownerCount, {
-											hidden: owner.count <= 1,
-										})}
-									>
-										×{owner.count}
-									</span>
-								</li>
-							))}
-						</ul>
-					) : (
-						<p className="text-center text-lighter text-xxs">
-							{t("trophies:details.noOwners")}
-						</p>
-					)}
 				</div>
 			</div>
 		</div>
