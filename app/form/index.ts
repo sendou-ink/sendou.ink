5610d567cb94bcb8ed5d5aee12c6b0102d9ea4cd
Kalle <38327916+Sendouc@users.noreply.github.com>
2026-07-31 14:21:36 +0300

    FIx

--- app/form/index.ts
+++ app/form/index.ts
@@ -1,10 +1,4 @@
 // Form system exports
 
-// Types
-export type {
-	FormDefaultValues,
-	FormMode,
-	FormRenderProps,
-} from "./SendouForm";
 export { SendouForm } from "./SendouForm";
 export type { ArrayItemRenderContext, CustomFieldRenderProps } from "./types";
