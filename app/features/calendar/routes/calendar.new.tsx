7a057b6b63367cd5ca27c62d35a07edec92e1d67
hfcRed <hfcred@gmx.net>
2026-09-17 15:50:23 -0400

    Add back style import

--- app/features/calendar/routes/calendar.new.tsx
+++ app/features/calendar/routes/calendar.new.tsx
@@ -33,6 +33,7 @@ import type { SendouRouteHandle } from "~/utils/remix.server";
 import { CREATING_TOURNAMENT_DOC_LINK, FAQ_PAGE } from "~/utils/urls";
 import { action } from "../actions/calendar.new.server";
 import type { RegClosesAtOption } from "../calendar-constants";
+import styles from "../calendar-new.module.css";
 import {
 	calendarNewBaseSchema,
 	customTeamPickPool,
